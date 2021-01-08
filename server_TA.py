9df10b8b026f805c265bbe1bdf762558e5a69c34
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-01-08 06:59:09 +0100

    Fixed rsu0 key

--- server_TA.py
+++ server_TA.py
@@ -3,12 +3,17 @@ import os, sys, socket
 class TA():
 
     def __init__(self):
-        RSU0 = ""
-        IDta = ""
-        private = ""
-        shared = ""
+        self.RSU0 = ""
+        self.IDta = ""
+        self.private = ""
+        self.shared = ""
 
+    def __str__(self):
+        return "__TA class__"
 
+    def __del__(self):
+        del self
+    ############################## getters
     def get_RSU0(self):
         return self.RSU0
 
@@ -21,26 +26,38 @@ class TA():
     def get_shared(self):
         return self.shared
 
-    ##############################
+    ############################## setters
     def set_RSU0(self, tmp):
         if tmp != self.RSU0:
-            selft.RSU0 = tmp
+            self.RSU0 = tmp
 
     def set_IDta(self, tmp):
         if tmp != self.IDta:
-            selft.IDta = tmp
+            self.IDta = tmp
 
     def set_private(self, tmp):
         if tmp != self.private:
-            selft.private = tmp
+            self.private = tmp
 
     def set_shared(self, tmp):
         if tmp != self.shared:
-            selft.shared = tmp
+            self.shared = tmp
+
+    ####################################################### functions
+    def refresh(self):
+        import random
+        alphacode = ["a", "b", "c", "1", "2", "3", "4", ]
+        tmp = ""
+        for i in range(10):
+            index = random.randint(0, len(alphacode)-1)
+            tmp += alphacode[index]
+
+        self.set_RSU0(tmp)
 
 
 if __name__ == '__main__':
 
+    ta = TA()
     host = "192.168.8.101"
     port = 12345
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,6 +71,7 @@ if __name__ == '__main__':
         data = conn.recv(1024)
         if not data: break
         print("======= data = ", data.decode())
+        ta.refresh()
+        conn.sendall(ta.get_RSU0().encode() + b": from server")
         print("=================================================")
-        conn.sendall(data + b"from server")
         conn.close()
